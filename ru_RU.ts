ced419df3c2173970e29cbf5dacde5ab9f235458
Evgeny Zakharov <jindos.lp@gmail.com>
2012-05-21 05:04:18 +0000

    modifed russificator

--- ru_RU.ts
+++ ru_RU.ts
@@ -2,6 +2,44 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
+    <name>graphics</name>
+    <message>
+        <location filename="graphics.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="graphics.cpp" line="13"/>
+        <source>dependence</source>
+        <translation>Зависимость X(количество) от Y(размер)</translation>
+    </message>
+    <message>
+        <location filename="graphics.cpp" line="43"/>
+        <source>First Graph</source>
+        <translation>Первые замеры</translation>
+    </message>
+    <message>
+        <location filename="graphics.cpp" line="47"/>
+        <source>Second Graph</source>
+        <translation>Вторые замеры</translation>
+    </message>
+    <message>
+        <location filename="graphics.cpp" line="51"/>
+        <source>All Graph</source>
+        <translation>Коэфициент</translation>
+    </message>
+    <message>
+        <location filename="graphics.cpp" line="72"/>
+        <source>size Space</source>
+        <translation>Размер</translation>
+    </message>
+    <message>
+        <location filename="graphics.cpp" line="74"/>
+        <source>number</source>
+        <translation>Число измерений</translation>
+    </message>
+</context>
+<context>
     <name>mainClass</name>
     <message>
         <location filename="mainclass.ui" line="17"/>
@@ -34,37 +72,37 @@
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="mainclass.cpp" line="48"/>
+        <location filename="mainclass.cpp" line="49"/>
         <source>All selected, now you can work</source>
         <translation>Можно выделять</translation>
     </message>
     <message>
-        <location filename="mainclass.cpp" line="57"/>
+        <location filename="mainclass.cpp" line="58"/>
         <source>Selected %1 Quads</source>
         <translation>Выделено %1 квадратов</translation>
     </message>
     <message>
-        <location filename="mainclass.cpp" line="90"/>
+        <location filename="mainclass.cpp" line="88"/>
         <source>Selected %1 quads</source>
         <translation>Выделено %1 квадратов</translation>
     </message>
     <message>
-        <location filename="mainclass.cpp" line="189"/>
+        <location filename="mainclass.cpp" line="187"/>
         <source>(+) Plus</source>
         <translation>(+) Плюс</translation>
     </message>
     <message>
-        <location filename="mainclass.cpp" line="193"/>
+        <location filename="mainclass.cpp" line="191"/>
         <source>(-) Minus</source>
         <translation>(-) Минус</translation>
     </message>
     <message>
-        <location filename="mainclass.cpp" line="201"/>
+        <location filename="mainclass.cpp" line="199"/>
         <source>WARNING</source>
         <translation>ВНИМАНИЕ</translation>
     </message>
     <message>
-        <location filename="mainclass.cpp" line="201"/>
+        <location filename="mainclass.cpp" line="199"/>
         <source>Too early, please select lines</source>
         <translation>Слишком рано,сначала выделите квадраты </translation>
     </message>
@@ -72,7 +110,7 @@
 <context>
     <name>parser</name>
     <message>
-        <location filename="parser.cpp" line="38"/>
+        <location filename="parser.cpp" line="69"/>
         <source>MISTAKE=%1</source>
         <translation>ОШИБКА=%1</translation>
     </message>
@@ -124,33 +162,42 @@
         <translation>Статистическая ошибка</translation>
     </message>
     <message>
+        <location filename="postoperations.ui" line="195"/>
+        <source>Create graphic</source>
+        <translation>Создать график</translation>
+    </message>
+    <message>
         <location filename="postoperations.ui" line="133"/>
         <source>Statistic</source>
         <translation>Статистика</translation>
     </message>
     <message>
-        <location filename="postoperations.cpp" line="38"/>
-        <location filename="postoperations.cpp" line="57"/>
+        <location filename="postoperations.cpp" line="48"/>
+        <location filename="postoperations.cpp" line="67"/>
         <source>Count spaces = %1</source>
         <translation>Количество подсчетов = %1</translation>
     </message>
     <message>
-        <location filename="postoperations.cpp" line="43"/>
-        <location filename="postoperations.cpp" line="45"/>
-        <location filename="postoperations.cpp" line="71"/>
-        <location filename="postoperations.cpp" line="73"/>
+        <location filename="postoperations.cpp" line="53"/>
+        <location filename="postoperations.cpp" line="55"/>
+        <location filename="postoperations.cpp" line="81"/>
+        <location filename="postoperations.cpp" line="83"/>
+        <location filename="postoperations.cpp" line="100"/>
+        <location filename="postoperations.cpp" line="102"/>
         <source>Select File</source>
         <translation>Выбрать файл</translation>
     </message>
     <message>
-        <location filename="postoperations.cpp" line="43"/>
-        <location filename="postoperations.cpp" line="71"/>
+        <location filename="postoperations.cpp" line="53"/>
+        <location filename="postoperations.cpp" line="81"/>
+        <location filename="postoperations.cpp" line="100"/>
         <source>Text files (*)</source>
         <translation>Текстовые файлы (*)</translation>
     </message>
     <message>
-        <location filename="postoperations.cpp" line="45"/>
-        <location filename="postoperations.cpp" line="73"/>
+        <location filename="postoperations.cpp" line="55"/>
+        <location filename="postoperations.cpp" line="83"/>
+        <location filename="postoperations.cpp" line="102"/>
         <source>Text files (*.txt)</source>
         <translation>Текстовые файлы (*.txt)</translation>
     </message>
